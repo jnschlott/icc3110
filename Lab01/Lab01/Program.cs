21b518290c1d996319995f4e155d823d1cd1544d
Juan Nicolás Schlotterbeck Byrne <jnschlotterbeck@miuandes.cl>
2013-08-10 17:47:42 -0300

    Lab01 - Principios del Gato

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablero t = new Tablero();
+            t.mostrarTablero();
+
+            int nu = 0;
+            int i = 1;
+            while (true)
+            {    
+            Console.WriteLine("Ingrese una posición:");
+                i = Int32.Parse(Console.ReadLine())-1;
+
+                if (nu == 0)
+	{
+                t.setPos(i,"X");
+                nu = 1;
+	}
+                if (nu == 0)
+	{
+                t.setPos(i,"O");
+                nu = 0;
+	}
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab01/Lab01/Tablero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Tablero
+    {
+        private string[] Pos = new string[9];
+        
+        public Tablero(){
+            for (int i = 1; i <= 9; i++)
+            {
+                Pos[i-1] = ""+i;
+            }
+        }
+
+
+        public void mostrarTablero(){
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine(" " + Pos[0] + " | " + Pos[1] + " | " + Pos[2] + " ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine(" " + Pos[3] + " | " + Pos[4] + " | " + Pos[5] + " ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine(" " + Pos[6] + " | " + Pos[7] + " | " + Pos[8] + " ");
+            Console.WriteLine("   |   |   ");
+        }
+
+        public void setPos(int i, string s)
+        {
+            Pos[i] = s;
+            mostrarTablero();
+        }
+    }
+}
